c273f7388a85b5386eb1645bd7dcdcbc256be426
Akash <45820053+akashsmu@users.noreply.github.com>
2020-11-10 20:25:16 +0530

    parallax

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,124 +1,25 @@
-import { Button } from "@material-ui/core";
 import React from "react";
-import styled from "styled-components";
-
-export default function InfoSection({
-  lightbg,
-  topLine,
-  lightText,
-  headLine,
-  darkText,
-  description,
-  buttonLabel,
-}) {
+import { Parallax } from "react-parallax";
+const inlineStyle = {
+  background: "#000",
+  left: "50%",
+  top: "50%",
+  position: "absolute",
+  padding: "20px",
+  transform: "translate(-50%, 50%)",
+};
+const image1 =
+  "https://images.unsplash.com/photo-1429153243204-48d8e868f40d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+export default function ParallaxPage() {
   return (
-    <>
-      {/* id={id} */}
-      <InfoContainer lightbg={lightbg}>
-        <InfoWrapper>
-          <InfoRow>
-            <Column1>
-              <TextWrapper>
-                <TopLines>{topLine}</TopLines>
-                <Headings lightText={lightText}>{headLine}</Headings>
-                <Subtitle darkText={darkText}>{description}</Subtitle>
-                <BtnWrap>
-                  <Button to="/">{buttonLabel}</Button>
-                </BtnWrap>
-              </TextWrapper>
-            </Column1>
-          </InfoRow>
-        </InfoWrapper>
-      </InfoContainer>
-    </>
+    <div>
+      <Parallax bgImage={image1} strength={400} bgImageSizes={"contain"}>
+        <div style={{ height: "450px", width: "100%" }}>
+          <div style={inlineStyle}>HTML inside Parallax</div>
+        </div>
+      </Parallax>
+      <div style={{ height: "100vh" }}></div>
+    </div>
   );
 }
 
-const InfoContainer = styled.div`
-  color: #fff;
-  background: ${({ lightbg }) => (lightbg ? "#f9f9f9" : "#010606")};
-  @media screen and (max-width: 768px) {
-    padding: 100px 0;
-  }
-`;
-
-const InfoWrapper = styled.div`
-  display: grid;
-  z-index: 1;
-  height: 860px;
-  width: 100%;
-  max-width: 100px;
-  margin-right: auto;
-  margin-left: auto;
-  padding: 0 24px;
-  justify-content: center;
-`;
-
-const InfoRow = styled.div`
-  display: grid;
-  grid-auto-columns: minmax(auto, 1fr);
-  align-items: center;
-  grid-template-areas: ${({ imgStart }) =>
-    imgStart ? `'col2 col1'` : `'col1 col2'`};
-  @media screen and (max-width: 768px) {
-    grid-template-areas: ${({ imgStart }) =>
-      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
-  }
-`;
-
-const Column1 = styled.div`
-  margin-bottom: 15px;
-  padding: 0 15p;
-  grid-area: col2;
-`;
-
-const TextWrapper = styled.div`
-  max-width: 540px;
-  padding-top: 0;
-  padding-bottom: 60px;
-`;
-
-const TopLines = styled.p`
-  color: #01bf71;
-  font-size: 16px;
-  line-height: 16px;
-  font-weight: 700;
-  letter-spacing: 1.4px;
-  text-transform: uppercase;
-  margin-bottom: 16px;
-`;
-
-const Headings = styled.h1`
-  margin-bottom: 24px;
-  font-size: 48px;
-  line-height: 1.1;
-  font-weight: 600;
-  color: ${({ lightText }) => (lightText ? "#f7f8fa" : "#010606")};
-  @media screen and (max-width: 480px) {
-    font-size: 32px;
-  }
-`;
-
-const Subtitle = styled.p`
-  max-width: 440px;
-  margin-bottom: 35px;
-  font-size: 18px;
-  line-height: 24px;
-  color: ${({ darkText }) => (darkText ? "#010606" : "#fff")};
-`;
-
-const BtnWrap = styled.div`
-  display: flex;
-  justify-content: flex-start;
-`;
-
-// const ImgWrap = styled.div`
-//   max-width: 555px;
-//   height: 100%;
-// `;
-
-// const Img = styled.img`
-//   width: 100%;
-//   margin: 0 0 10px 0;
-//   padding-right: 0;
-// `;
